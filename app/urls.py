6e38eeaaa79cd77ff8ed5a88cc85effefc480179
rnvarma <rohan@prompthire.com>
2016-11-03 19:22:43 -0400

    added js

--- app/urls.py
+++ app/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from app.views import *
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', HomePage.as_view())
 ]
